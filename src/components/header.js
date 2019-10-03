4a3c425b451ac110b65c698b1701a6bb16036b6a
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-10-03 19:43:16 +0100

    added links

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { css } from "@emotion/core"
 import { colors, fonts } from "gatsby-design-tokens"
 import { rhythm } from "../utils/typography"
 import { Link } from "gatsby"
-
+import { FaMedium, FaTwitter, FaGithub, FaLinkedin, FaLink } from 'react-icons/fa';
 
 // https://news.ycombinator.com/item?id=21050501
 // https://codeburst.io/flexbox-building-a-navigation-part-2-2-6cc58b9d4173
@@ -12,11 +12,14 @@ import headerStyles from "./header.module.css"
 
 
 const NavLiItem = styled(`li`)`
-  color: ${colors.purple[50]};
+  color: black;
   margin-bottom:  ${rhythm(1 / 2)};
   margin-top:  ${rhythm(1 / 2)};
   font-family: ${fonts.monospace.join(", ")};
+`
 
+const BlackA = styled(`a`)`
+  color: black;
 `
 
 const mono = css`
@@ -30,9 +33,21 @@ const Header = () => (
       <ul>
       <NavLiItem css={mono} className={headerStyles.robin}><Link to="/">>robinlinacre</Link></NavLiItem>
       <NavLiItem className={headerStyles.middlespace}></NavLiItem>
-      <NavLiItem >About</NavLiItem>
-      <NavLiItem >Medium</NavLiItem>
-      <NavLiItem >Twitter</NavLiItem>
+
+
+        <NavLiItem >
+          <BlackA href="https://github.com/robinl/"><FaGithub /></BlackA>
+        </NavLiItem>
+        <NavLiItem >
+          <BlackA href="https://medium.com/@robin.linacre"><FaMedium /></BlackA>
+        </NavLiItem>
+        <NavLiItem >
+          <BlackA href="https://www.linkedin.com/in/robinlinacre/"><FaLinkedin /></BlackA>
+        </NavLiItem>
+        <NavLiItem >
+          <BlackA href="https://twitter.com/RobinLinacre"><FaTwitter /></BlackA>
+        </NavLiItem>
+
       </ul>
     </nav>
     <hr/>
