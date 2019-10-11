40de548f2782196e49a4d1870ef8b8eb79d8f466
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-10-11 15:13:52 +0100

    fixed post date

--- src/pages/transforming_analytical_functions.jsx
+++ src/pages/transforming_analytical_functions.jsx
@@ -4,7 +4,7 @@ import MDXDocument from "../mdx/transforming_analytical_functions"
 
 export const frontmatter = {
   title: "Transforming analytical functions by mainstreaming data science",
-  post_date: "2018-08-1",
+  post_date: "2018-08-11",
   code_url:
     "https://github.com/RobinL/robinlinacre/blob/dev/src/pages/transforming_analytical_functions.mdx",
   post_type: "mdx",
