c4b2edd21958d516c3a378d8f894d7d01c4db543
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-02-22 15:14:22 +0000

    open source

---
+++ src/pages/open_sourcing_analytical_work.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ObsMdxPage from "../components/mdx_obs_page"
+import MDXDocument from "../mdx/open_sourcing_analytical_work.mdx"
+
+export const frontmatter = {
+         title: "Why you should open source your analytical work",
+         post_date: "2020-02-22",
+         code_url:
+           "https://github.com/RobinL/robinlinacre/blob/dev/src/pages/open_sourcing_analytical_work.mdx",
+         post_type: "mdx",
+         post_category: "data",
+       }
+
+export default ({ data }) => (
+    <ObsMdxPage post_frontmatter={frontmatter}>
+        <MDXDocument />
+    </ObsMdxPage>
+)
