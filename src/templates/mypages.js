fbf46c58d2e32422ee134b9496a08cba1e876c5d
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2021-05-06 20:51:54 +0100

    update site

--- src/templates/mypages.js
+++ src/templates/mypages.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Layout from "../layouts/"
-export default ({data}) => {
+import { graphql } from 'gatsby'
 
-    return (
-        <Layout>
-            <div>
-                <p>The id for this page is: {data.mydataJson.myid}</p>
-                <p>The content for this page is: {data.mydataJson.p}</p>
-            </div>
-        </Layout>
-    )
+export default ({ data }) => {
+
+  return (
+    <Layout>
+      <div>
+        <p>The id for this page is: {data.mydataJson.myid}</p>
+        <p>The content for this page is: {data.mydataJson.p}</p>
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
