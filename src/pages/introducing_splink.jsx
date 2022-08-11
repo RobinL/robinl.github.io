1b37d9743ec33011ceac863353a1733b6e10e1dd
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-08-11 11:01:45 +0100

    update date

--- src/pages/introducing_splink.jsx
+++ src/pages/introducing_splink.jsx
@@ -4,12 +4,12 @@ import MDXDocument from "../mdx/introducing_splink.mdx"
 
 export const frontmatter = {
          title: "Fuzzy Matching and Deduplicating Hundreds of Millions of Records with Splink",
-         post_date: "2020-04-16",
+         post_date: "2022-08-04",
          code_url:
            "https://github.com/RobinL/robinl.github.io/blob/dev/src/mdx/introducing_splink.mdx",
          post_type: "mdx",
          post_category: "data",
-         description: "Introducing Splink, a fast, accurate and scalable record linkage library that supports multiple SQL backends"
+         description: "Introducing Splink, a fast, accurate and scalable fuzzy record matching library that supports multiple SQL backends"
        }
 
 export default ({ data }) => (
