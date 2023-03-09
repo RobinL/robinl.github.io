0673d4927ad4ec40dae435be02a3becd7514f32e
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-03-09 17:45:00 +0000

    ready for pub

--- src/pages/open_source_dividend.jsx
+++ src/pages/open_source_dividend.jsx
@@ -3,8 +3,8 @@ import ObsMdxPage from "../components/mdx_obs_page"
 import MDXDocument from "../mdx/open_source_dividend.mdx"
 
 export const frontmatter = {
-         title: "The open source dividend",
-         post_date: "2023-03-04",
+         title: "Splink and the Open Source Dividend",
+         post_date: "2023-03-09",
          code_url:
            "https://github.com/RobinL/robinl.github.io/blob/dev/src/mdx/open_source_dividend.mdx",
          post_type: "mdx",
