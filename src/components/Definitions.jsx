7679b4f6e1c2329c2f0943f93f99bf1266e8b11c
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-13 14:33:00 +0100

    partial match weight definition

--- src/components/Definitions.jsx
+++ src/components/Definitions.jsx
@@ -7,3 +7,10 @@ export const prior = (
         definition="A prior is a probability that represents our beliefs about the value of a parameter before we have seen any data."
     />
 );
+
+export const partial_match_weight = (
+    <DefinitionTooltip
+        term="partial match weight"
+        definition="The partial match weights represent the relative importance of different pieces of information in determining whether two records are a match. For example, a match on date of birth is more important than a match on gender.  They are defined as the log 2 (m/u), where m and u represent the m and u probabilities respectively."
+    />
+);
