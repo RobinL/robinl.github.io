1c7c481fb15342483c866303c2ee2e16219651e0
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-26 09:44:33 +0100

    updates

--- python/match_weights_article/generate_bf_movement_chart.py
+++ python/match_weights_article/generate_bf_movement_chart.py
@@ -11,11 +11,11 @@ from splink.misc import (
 
 
 def format_to_2sf(number):
-    return "{:g}".format(float(f"{number:.2g}"))
+    return "{:g}".format(float(f"{number:.3g}"))
 
 
-pr_mw = -2
-po_mw = 3.5
+pr_mw = 2
+po_mw = -5.5
 
 pr_bf = match_weight_to_bayes_factor(pr_mw)
 po_bf = match_weight_to_bayes_factor(po_mw)
@@ -33,6 +33,8 @@ data = [
         "posterior_bayes_factor_fmt": format_to_2sf(po_bf),
         "prior_prob_fmt": format_to_2sf(pr_prob),
         "posterior_prob_fmt": format_to_2sf(po_prob),
+        "prior_text": "prior",
+        "posterior_text": "posterior",
     }
 ]
 
@@ -51,13 +53,32 @@ mark_text_args = {
     "fontSize": 16,
 }
 
+text_arrow = (
+    alt.Chart(data)
+    .mark_text(baseline="middle", fontWeight="bold", dx=0, fontSize=30, color="white")
+    .transform_calculate(
+        midpoint="(datum.prior_match_weight + datum.posterior_match_weight) / 2"
+    )
+    .encode(
+        x=alt.X("midpoint:Q"),
+        text=alt.condition(
+            "datum.prior_match_weight < datum.posterior_match_weight",
+            alt.value("→"),
+            alt.value("←"),
+        ),
+    )
+)
+
+
 line_height = 30
+
+
 text1a = (
     alt.Chart(data)
     .mark_text(**mark_text_args)
     .encode(
         x=alt.X("prior_match_weight:Q", axis=axis1),
-        text="prior_match_weight:Q",
+        text="prior_prob_fmt:Q",
     )
 )
 
@@ -66,74 +87,115 @@ text1b = (
     .mark_text(**mark_text_args)
     .encode(
         x=alt.X("posterior_match_weight:Q", axis=axis1),
-        text="posterior_match_weight:Q",
+        text="posterior_prob_fmt:Q",
     )
 )
 
-# LN2
-# format(datum.value/(1-datum.value), ',.1f')
+
 axis2 = alt.Axis(title="Partial Match Weight", labelExpr=f"datum.value")
 
 
-text2 = (
+text2a = (
     alt.Chart(data)
     .mark_text(**mark_text_args)
     .encode(
         x=alt.X("prior_match_weight:Q", axis=axis2),
-        text="prior_match_weight:Q",
+        text="prior_match_weight_fmt:Q",
     )
 )
+
+text2b = (
+    alt.Chart(data)
+    .mark_text(**mark_text_args)
+    .encode(
+        x=alt.X("posterior_match_weight:Q", axis=axis2),
+        text="posterior_match_weight_fmt:Q",
+    )
+)
+
+text2c = (
+    alt.Chart(data)
+    .mark_text(**mark_text_args)
+    .encode(
+        x=alt.X("prior_match_weight:Q", axis=axis2),
+        text=alt.value("prior"),
+    )
+)
+
+text2d = (
+    alt.Chart(data)
+    .mark_text(**mark_text_args)
+    .encode(
+        x=alt.X("posterior_match_weight:Q", axis=axis2),
+        text=alt.value("posterior"),
+    )
+)
+
 
 axis3 = alt.Axis(
     title="Bayes Factor (odds)",
     labelExpr="datum.value < 1 ? format(pow(2, datum.value),',.3f') : format(pow(2, datum.value),',.0f') ",
 )
 
-text3 = (
+text3a = (
     alt.Chart(data)
     .mark_text(**mark_text_args)
     .encode(
         x=alt.X("prior_match_weight:Q", axis=axis3),
-        text="bf_fmt:Q",
+        text="prior_bayes_factor_fmt:Q",
+    )
+)
+
+text3b = (
+    alt.Chart(data)
+    .mark_text(**mark_text_args)
+    .encode(
+        x=alt.X("posterior_match_weight:Q", axis=axis3),
+        text="posterior_bayes_factor_fmt:Q",
     )
 )
 
 
 bar = (
     alt.Chart(data)
-    .mark_bar(opacity=0.5)
+    .mark_bar()
     .encode(
         x=alt.X("prior_match_weight:Q", scale=alt.Scale(domain=[-10, 10])),
         x2=alt.X2("posterior_match_weight:Q"),
+        color=alt.value("#4FABF5"),
     )
 )
 
 
-c1 = alt.layer(bar, text1a, text1b).properties(width=450, height=line_height)
+c1 = alt.layer(bar, text1a, text1b, text_arrow).properties(
+    width=450, height=line_height
+)
+c1b = alt.layer(bar, text2c, text2d, text_arrow).properties(
+    width=450, height=line_height
+)
 
-c2 = alt.layer(
-    bar,
-    text2,
-).properties(width=450, height=line_height)
+c2 = alt.layer(bar, text2a, text2b, text_arrow).properties(
+    width=450, height=line_height
+)
 
-c3 = alt.layer(
-    bar,
-    text3,
-).properties(width=450, height=line_height)
+c3 = alt.layer(bar, text3a, text3b, text_arrow).properties(
+    width=450, height=line_height
+)
 
 
 final_chart = (
-    alt.vconcat(c1, c2, c3)
+    alt.vconcat(c1b, c2, c3, c1)
     .properties(
         title="Updating a prior probability of x with a partial match weight of y"
     )
     .configure_title(anchor="middle")
 )
-# # get spec as dict
-# final_chart_spec = final_chart.to_dict()
 
 
-# # Save final_chart_spec to json pretty
-# with open("../../src/mdx/m_and_u_probabilities/spec.json", "w") as f:
-#     json.dump(final_chart_spec, f, indent=4)
-final_chart
+final_chart_spec = final_chart.to_dict()
+
+
+# Save final_chart_spec to json pretty
+with open("../../src/mdx/m_and_u_probabilities/bf_movement_spec.json", "w") as f:
+    json.dump(final_chart_spec, f, indent=4)
+final_chart_spec
