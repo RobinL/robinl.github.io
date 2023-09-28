acc23b6d386c30c9996d44a897ccb13298703c28
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-28 14:27:30 +0100

    add definitions

--- src/components/Definitions.jsx
+++ src/components/Definitions.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import DefinitionTooltip from './DefinitionToolTip';
+import DefinitionToolTip from './DefinitionToolTip';
 
-export const prior = (
-    <DefinitionTooltip
-        term="prior"
-        definition="A prior is a probability that represents our beliefs about the value of a parameter before we have seen any data."
-    />
+export const prior = () => <DefinitionToolTip term="prior" />;
+
+export const partial_match_weight = () => (
+    <DefinitionToolTip term="partial_match_weight" />
 );
 
-export const partial_match_weight = (
-    <DefinitionTooltip
-        term="partial match weight"
-        definition="The partial match weights represent the relative importance of different pieces of information in determining whether two records are a match. For example, a match on date of birth is more important than a match on gender.  They are defined as the log 2 (m/u), where m and u represent the m and u probabilities respectively."
-    />
-);
+export const scenario = () => <DefinitionToolTip term="scenario" />;
