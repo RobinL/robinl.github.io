96c90798322318ed707a5756a4f84b6e86c4e4c8
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-30 22:29:43 +0100

    tooltip before

--- src/components/DefinitionToolTip.jsx
+++ src/components/DefinitionToolTip.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const getData = () => {
@@ -27,6 +27,42 @@ const getData = () => {
 
 const DefinitionToolTip = ({ term }) => {
     const [showDefinition, setShowDefinition] = useState(false);
+    const tooltipRef = useRef(null);
+
+    useEffect(() => {
+        if (showDefinition && tooltipRef.current) {
+            const rect = tooltipRef.current.getBoundingClientRect();
+            const style = tooltipRef.current.style;
+            let transformX = '-50%';
+            let transformY = '';
+
+            // Check right overflow
+            if (rect.right > window.innerWidth) {
+                const overflow = rect.right - window.innerWidth;
+                transformX = `calc(-50% - ${overflow}px)`;
+            }
+            // Check left overflow
+            else if (rect.left < 0) {
+                transformX = `-calc(50% + ${Math.abs(rect.left)}px)`;
+            }
+
+            // Check top overflow
+            if (rect.top < 0) {
+                style.top = '100%';
+                style.bottom = 'auto';
+                transformY = '10px';
+            }
+            // Check bottom overflow
+            else if (rect.bottom > window.innerHeight) {
+                style.bottom = '100%';
+                style.top = 'auto';
+                transformY = '-10px';
+            }
+
+            // Apply transformations
+            style.transform = `translate(${transformX}, ${transformY})`;
+        }
+    }, [showDefinition]);
 
     const toggleDefinition = () => {
         setShowDefinition(!showDefinition);
@@ -51,7 +87,10 @@ const DefinitionToolTip = ({ term }) => {
                 {term}
             </span>
             {showDefinition && (
-                <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-blue-50 border border-gray-300 p-2 w-64 text-left text-xs z-10 definition-tooltip-content">
+                <div
+                    ref={tooltipRef}
+                    className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-blue-50 border border-gray-300 p-2 w-64 text-left text-xs z-10 definition-tooltip-content"
+                >
                     <div
                         dangerouslySetInnerHTML={{ __html: filteredHtml }}
                     ></div>
