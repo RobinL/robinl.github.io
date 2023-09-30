f40f9691d9f67b91abc3ba71a07ac5d1bbacdfaf
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-09-30 23:15:32 +0100

    update

--- src/components/DefinitionToolTip.jsx
+++ src/components/DefinitionToolTip.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
+import { Tooltip } from 'react-tooltip';
+
 import { graphql, useStaticQuery } from 'gatsby';
 
 const getData = () => {
@@ -26,52 +28,8 @@ const getData = () => {
 };
 
 const DefinitionToolTip = ({ term }) => {
-    const [showDefinition, setShowDefinition] = useState(false);
-    const tooltipRef = useRef(null);
-
-    useEffect(() => {
-        if (showDefinition && tooltipRef.current) {
-            const rect = tooltipRef.current.getBoundingClientRect();
-            const style = tooltipRef.current.style;
-            let transformX = '-50%';
-            let transformY = '';
-
-            // Check right overflow
-            if (rect.right > window.innerWidth) {
-                const overflow = rect.right - window.innerWidth;
-                transformX = `calc(-50% - ${overflow}px)`;
-            }
-            // Check left overflow
-            else if (rect.left < 0) {
-                transformX = `-calc(50% + ${Math.abs(rect.left)}px)`;
-            }
-
-            // Check top overflow
-            if (rect.top < 0) {
-                style.top = '100%';
-                style.bottom = 'auto';
-                transformY = '10px';
-            }
-            // Check bottom overflow
-            else if (rect.bottom > window.innerHeight) {
-                style.bottom = '100%';
-                style.top = 'auto';
-                transformY = '-10px';
-            }
-
-            // Apply transformations
-            style.transform = `translate(${transformX}, ${transformY})`;
-        }
-    }, [showDefinition]);
-
-    const toggleDefinition = () => {
-        setShowDefinition(!showDefinition);
-    };
-
     const data = getData();
-
     const filteredNode = data.find(edge => edge.node.frontmatter.term === term);
-
     const filteredHtml = filteredNode
         ? filteredNode.node.html
         : 'Term not found';
@@ -79,23 +37,27 @@ const DefinitionToolTip = ({ term }) => {
     return (
         <span className="relative">
             <span
-                className="underline cursor-pointer"
-                onMouseEnter={toggleDefinition}
-                onMouseLeave={toggleDefinition}
-                onClick={toggleDefinition}
+                className="underline cursor-pointer blahh"
+                data-tip
+                data-for={`tooltip-${term}`}
             >
                 {term}
             </span>
-            {showDefinition && (
+            <Tooltip
+                id={`tooltip-${term}`}
+                anchorSelect=".blahh"
+                style={{
+                    backgroundColor: '#EEE',
+                    color: '#222',
+                    fontSize: '0.8rem',
+                }}
+                clickable
+            >
                 <div
-                    ref={tooltipRef}
-                    className="absolute bottom-full left-1/2 transform -translate-x-1/2 bg-blue-50 border border-gray-300 p-2 w-64 text-left text-xs z-10 definition-tooltip-content"
-                >
-                    <div
-                        dangerouslySetInnerHTML={{ __html: filteredHtml }}
-                    ></div>
-                </div>
-            )}
+                    className="w-64 text-xs z-10 definition-tooltip-content"
+                    dangerouslySetInnerHTML={{ __html: filteredHtml }}
+                ></div>
+            </Tooltip>
         </span>
     );
 };
--- src/definitions_markdown/prior.md
+++ src/definitions_markdown/prior.md
@@ -3,3 +3,7 @@ term: 'prior'
 ---
 
 The 'prior' is a probability that represents our beliefs about the value of a parameter before we have seen any data.
+
+In the context of record linkage models, it's often used to refer to our belief of the probability that two records picked at random from the input data will match.
+
+In Splink, this parameter is called the `probability_two_random_records_match` in the settings object.
