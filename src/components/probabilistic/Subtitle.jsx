54d3b686375eb67afb755b93b29fda9220e1eec0
Robin Linacre <robinlinacre@hotmail.com>
2026-04-18 10:29:09 +0100

    add articles

--- src/components/probabilistic/Subtitle.jsx
+++ src/components/probabilistic/Subtitle.jsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
 export default function Subtitle({ children }) {
-  const content = React.Children.map(children, (child) => {
-    if (React.isValidElement(child) && child.type === 'p') return child.props.children;
-    return child;
-  });
-
-  return <p className="post-subtitle">{content}</p>;
+  return <div className="post-subtitle">{children}</div>;
 }
-
